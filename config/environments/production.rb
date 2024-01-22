c57e7541ab5725986965eecf95d8edacdad142a9
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-01-22 23:28:43 +0900

    herokuへのデプロイ準備

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'rails-ec-mutsuki-45372128c0a6.herokuapp.com' }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'tranquil-lake-26819-cc4d9a316040.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
