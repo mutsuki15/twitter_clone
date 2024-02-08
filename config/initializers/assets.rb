d875f8605d58016f107c024db125cc7222f3dabf
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-02-08 22:47:20 +0900

    Sassロードパスを追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
 Rails.application.config.assets.precompile += %w( application.bootstrap.css )
+Rails.application.config.assets.paths << Rails.root.join('node_modules', 'bootstrap', 'scss')
+
 
 
 # Precompile additional assets.
